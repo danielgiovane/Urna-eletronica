4742c762a35d2237b60ef4b370fe06e68606020d
Daniel Fernandes <danielgiovane2015@gmail.com>
2020-09-17 19:33:52 -0300

    versão 0.5, Criando função ComecarEtapa() e templateNumeros().

--- public/app.js
+++ public/app.js
@@ -1,14 +1,37 @@
 import { dados } from './dados-urna/dados.js'
 
 //Variaveis de controle de interface
-const botoes = document.querySelectorAll('.botao');
-const informacoes = document.querySelector('.informacoes').classList.add('info-oculta')
-const seuVoto = document.querySelector('.texto-inicial span').classList.add('info-oculta')
-const aviso = document.querySelector('.rodape').classList.add('info-oculta');
-const infoLateral = document.querySelector('.cabecalho-direito').classList.add('info-oculta')
-const numeros = document.querySelector('.numeros').classList.add('info-normal')
+let botoes = document.querySelectorAll('.botao');
+let informacoes = document.querySelector('.informacoes');
+let seuVoto = document.querySelector('.texto-inicial span');
+let cargo = document.querySelector('.cargo-votado span');
+let aviso = document.querySelector('.rodape');
+let infoLateral = document.querySelector('.cabecalho-direito');
+let caixaDosNumeros = document.querySelector('.numeros');
 
 // Variaveis de controle de ambiente
+
+let etapaAtual = 0;
+
+function comecarEtapa() {
+  let numeroHtml = '';
+  const dado = dados[etapaAtual];
+  cargo.innerHTML = dado.titulo;
+  informacoes.innerHTML = '';
+  seuVoto.innerHTML = '';
+  aviso.style.display = 'none'
+  infoLateral.innerHTML = '';
+  caixaDosNumeros.innerHTML = templateNumeros(dado, numeroHtml);
+}
+
+function templateNumeros(dado, numeroHtml) {
+  for (let i = 0; i < dado.numero; i++) {
+    numeroHtml += '<div class="numero"></div>'
+  }
+  return numeroHtml;
+}
+
+
 
 function confirmar(dados) {
 
@@ -34,6 +57,7 @@ function eventoDeClick(botoes) {
   })
 }
 
+comecarEtapa();
 eventoDeClick(botoes)
 
 
