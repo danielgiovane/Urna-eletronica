8715d5c64a97b9b9c764cc5f7c6ef2f040304b35
Daniel Fernandes <danielgiovane2015@gmail.com>
2020-09-17 14:59:54 -0300

    Versão 0.4, Configurando variaveis de interface

--- public/app.js
+++ public/app.js
@@ -1,11 +1,40 @@
-import {dados} from './dados.js'
+import { dados } from './dados-urna/dados.js'
 
-const numeros = document.querySelectorAll('.botao');
+//Variaveis de controle de interface
+const botoes = document.querySelectorAll('.botao');
+const informacoes = document.querySelector('.informacoes').classList.add('info-oculta')
+const seuVoto = document.querySelector('.texto-inicial span').classList.add('info-oculta')
+const aviso = document.querySelector('.rodape').classList.add('info-oculta');
+const infoLateral = document.querySelector('.cabecalho-direito').classList.add('info-oculta')
+const numeros = document.querySelector('.numeros').classList.add('info-normal')
 
-function obtendoNumeros(e){
-  let elemento = e.target.innerText;
-  alert(elemento);
+// Variaveis de controle de ambiente
+
+function confirmar(dados) {
+
 }
-numeros.forEach(function(numero){
-  numero.addEventListener('click', obtendoNumeros)
-})
+
+function corrigir() {
+
+}
+
+function votoEmBranco() {
+
+}
+
+function obtendoNumeros(e) {
+  let elemento = e.target.innerText;
+  console.log(elemento)
+  return elemento;
+}
+
+function eventoDeClick(botoes) {
+  return botoes.forEach(function (numeroDoBotao) {
+    numeroDoBotao.addEventListener('click', obtendoNumeros)
+  })
+}
+
+eventoDeClick(botoes)
+
+
+
