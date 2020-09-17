01a544d5614e7a6538923cf025c02491f56a4bb2
Daniel Fernandes <danielgiovane2015@gmail.com>
2020-09-17 14:17:20 -0300

    versão 0.3, obtendo numeros digitados na tela

--- public/app.js
+++ public/app.js
@@ -1,2 +1,11 @@
 import {dados} from './dados.js'
 
+const numeros = document.querySelectorAll('.botao');
+
+function obtendoNumeros(e){
+  let elemento = e.target.innerText;
+  alert(elemento);
+}
+numeros.forEach(function(numero){
+  numero.addEventListener('click', obtendoNumeros)
+})
